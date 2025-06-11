bf57e9314809c6c22371ce202a57e94aa02772ef
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2025-06-11 16:14:00 +0800

    20250611_5
    20250611_5

---
+++ 20250516/04_PX_byHeritance.cpp
@@ -0,0 +1,91 @@
+/*
+	(1) little characters, a,b,c are used for object instances
+	(2) big characters A,B,C are used for matrix
+*/
+
+#include <iostream>
+
+template <typename T>
+class Matrix
+{
+public:
+	T A;		// the first type of matrix
+	int rows, columns;	// rows=n; columns=m;
+	Matrix(int n, int m):rows(n),columns(m){
+		A=new int*[n];
+		for(int i=0; i< n; i++)
+			A[i]=new int[m];
+		int k=0;	
+		for(int i=0; i<n; i++)
+		for(int j=0; j<m; j++)
+			A[i][j]	=k++;
+	}
+	void printMatrixDimension(){
+		std::cout<<"The matrix dimensions n=" << rows<< " m= "<<columns<<std::endl;
+	}
+	
+	void printMatrix(){
+		for(int i=0; i<rows; i++){
+			for(int j=0; j<columns; j++) std::cout<< A[i][j]	<< " ";	
+			std::cout<<std::endl;			
+		}	
+	}
+	
+	void deleteMatrix(){
+	    for (int i = 0; i < rows; ++i) delete[] A[i]; // Release memory and Ball destruBtor 
+		delete[] A; // Release memory and Ball destruBtor		
+	} 
+	
+};
+
+
+template<typename T>
+class AxB: public Matrix<T>{
+	public:
+	T C; 	// the second type of matrix
+	int rows,columns;
+	AxB(Matrix<T>& a, Matrix<T>& b) : Matrix<T>(a.rows, b.columns) 
+    {        // passing by reference
+		a.printMatrixDimension();
+		a.printMatrix();
+		b.printMatrixDimension();
+		b.printMatrix();
+		
+		rows=a.rows; columns=b.columns;
+			
+		// The type T in this context will be consistently used as 
+		// int** throughout the class methods.
+		Matrix<T> c(rows,columns);	// create object c
+
+		T A=a.A, B=b.A; C=c.A;
+		for(int i=0; i<a.rows; i++){
+			for(int j=0; j<b.columns; j++){
+				C[i][j]=0;
+				for(int k=0; k<a.columns; k++)
+					C[i][j] +=A[i][k]*B[k][j];
+			}
+		}
+		c.printMatrixDimension();
+		c.printMatrix();	
+	}
+	void deleteMatrix(){
+	    for (int i = 0; i < rows; ++i) delete[] C[i]; // Release memory and Ball destruBtor 
+	    delete[] C ; // Release memory and Ball destruBtor 
+	} 	
+	
+}; 
+
+int main() {
+	Matrix<int**> a(2,3);	// create object a
+	Matrix<int**> b(3,2);	// create object a
+	
+	AxB<int**> c(a,b);
+
+	a.deleteMatrix();
+	b.deleteMatrix();
+	c.deleteMatrix();
+	
+
+	
+	return 0;
+}
